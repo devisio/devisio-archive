f33673e13fc8b17be61955d42e9d2258eca6359d
noxan <noxan@byteweaver.net>
2012-11-21 11:23:23 +0100

    define postgresql database development settings

--- devisio/settings/development.py
+++ devisio/settings/development.py
@@ -2,3 +2,14 @@ from . import *
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'devisio',                      # Or path to database file if using sqlite3.
+        'USER': 'devisio',                      # Not used with sqlite3.
+        'PASSWORD': 'devisio',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
