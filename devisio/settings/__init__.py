f7cb8d81c5a86b945952b9cc1c92c91023d908c9
noxan <noxan@byteweaver.net>
2012-11-21 22:52:19 +0100

    set media and static root folders in settings

--- devisio/settings/__init__.py
+++ devisio/settings/__init__.py
@@ -49,7 +49,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = path.join(PROJECT_PATH, 'deploy/media/')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -60,7 +60,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = path.join(PROJECT_PATH, 'deploy/static/')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
