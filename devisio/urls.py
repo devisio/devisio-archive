d726ca9cac2227e590d59b6f5781e1f6fc6eaf4b
noxan <noxan@byteweaver.net>
2012-11-21 21:06:37 +0100

    update urls to display home template on home view

--- devisio/urls.py
+++ devisio/urls.py
@@ -6,7 +6,7 @@ from django.views.generic import TemplateView
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
+    url(r'^$', TemplateView.as_view(template_name='devisio/home.html'), name='home'),
 
     # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
