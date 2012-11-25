638d7aa896ed662e19c81de6d0968a6a172a7db7
noxan <noxan@byteweaver.net>
2012-11-25 23:50:52 +0100

    create template and url for contact page

--- devisio/urls.py
+++ devisio/urls.py
@@ -9,6 +9,7 @@ urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='devisio/home.html'), name='home'),
     url(r'^about/$', TemplateView.as_view(template_name='devisio/about.html'), name='about'),
     url(r'^members/$', TemplateView.as_view(template_name='devisio/members.html'), name='members'),
+    url(r'^contact/$', TemplateView.as_view(template_name='devisio/contact.html'), name='contact'),
 
     # admin
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
