80b200f9f69d8e4ca9b46ac57bc25dd474793252
noxan <noxan@byteweaver.net>
2012-11-28 14:32:04 +0100

    install grappelli to settings and urls

--- devisio/settings/__init__.py
+++ devisio/settings/__init__.py
@@ -116,6 +116,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # admin
+    'grappelli',
     'django.contrib.admin',
     'django.contrib.admindocs',
 )
