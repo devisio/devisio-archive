48dc1dbc85d19db969772184b0455b963ca8feea
noxan <noxan@byteweaver.net>
2012-11-28 20:42:07 +0100

    implement basic account model and manager (like in the tutorial)

--- devisio/accounts/models.py
+++ devisio/accounts/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager
+from django.utils.translation import ugettext_lazy as _
 
-# Create your models here.
+
+class AccountManager(BaseUserManager):
+    pass
+
+
+class Account(AbstractBaseUser):
+    email = models.EmailField(_("Email address"), unique=True)
+
+    objects = AccountManager()
+
+    USERNAME_FIELD = 'email'
+
+    def get_full_name(self):
+        return self.email
+
+    def get_short_name(self):
+        return self.email
+
+    def __unicode__(self):
+        return self.email
+
+    def has_perm(self, perm, obj=None):
+        return True
+
+    def has_module_perms(self, app_label):
+        return True
+
+    @property
+    def is_staff(self):
+        return True
