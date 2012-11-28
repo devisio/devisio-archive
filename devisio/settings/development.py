242aff670256809ccbaa02f9bc537670b8ab66dc
noxan <noxan@byteweaver.net>
2012-11-28 21:45:37 +0100

    add django extensions to installed apps in development settings

--- devisio/settings/development.py
+++ devisio/settings/development.py
@@ -13,3 +13,5 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
+
+INSTALLED_APPS = INSTALLED_APPS + ('django_extensions',)
