3130f9cff969991efc71e6d3c881facce0dc2987
noxan <noxan@byteweaver.net>
2012-11-29 20:41:45 +0100

    add basic album model to photos app

--- devisio/photos/models.py
+++ devisio/photos/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Album(models.Model):
+    name = models.CharField(max_length=255)
+    owner = models.ForeignKey(User)
+    create_date = models.DateTimeField(auto_now_add=True, editable=False)
+    edit_date = models.DateTimeField(auto_now=True, editable=False)
