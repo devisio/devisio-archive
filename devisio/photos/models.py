8dabcb815048b4e6e13cff0f8544b1619950c86f
noxan <noxan@byteweaver.net>
2012-11-29 22:36:21 +0100

    add slug field to album model

--- devisio/photos/models.py
+++ devisio/photos/models.py
@@ -8,6 +8,7 @@ from filebrowser.fields import FileBrowseField
 
 class Album(models.Model):
     name = models.CharField(max_length=255)
+    slug = models.SlugField()
     owner = models.ForeignKey(settings.AUTH_USER_MODEL, editable=False)
     date = models.DateField(default=timezone.now())
 
