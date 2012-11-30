44cd77c00ffa0abe1acd8658a5780c91ad45967f
noxan <noxan@byteweaver.net>
2012-11-30 14:13:49 +0100

    hook upload photos link to album admin list view

--- devisio/photos/admin.py
+++ devisio/photos/admin.py
@@ -5,8 +5,12 @@ from models import Album
 
 
 class AlbumAdmin(admin.ModelAdmin):
-    list_display = ('name', 'owner', 'visible')
+    list_display = ('name', 'owner', 'visible', 'photos')
     prepopulated_fields = {'slug': ('name',)}
+
+    def photos(self, obj):
+        return '<ul class="grp-actions"><li class="grp-change-link"><a href="/admin/filebrowser/upload/?&dir='+obj.get_path()+'" class="">Add photos</a></li></ul>'
+    photos.allow_tags = True
 
     def save_model(self, request, obj, form, change):
         try:
