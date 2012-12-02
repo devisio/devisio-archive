7e12bee9e5b70a8854f153f408b2f5e2f2b21284
noxan <noxan@byteweaver.net>
2012-12-02 12:43:11 +0100

    update production database settings and remove dj-database-url

--- devisio/settings/production.py
+++ devisio/settings/production.py
@@ -1,8 +1,16 @@
 from . import *
-import dj_database_url
 
 
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-DATABASES['default'] =  dj_database_url.config()
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': 'devisio',                      # Or path to database file if using sqlite3.
+        'USER': 'devisio',                      # Not used with sqlite3.
+        'PASSWORD': '@8.b3D3&a^/r4,WTM5l4/*6}',                  # Not used with sqlite3.
+        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    }
+}
