6b3f511fc965125d46501a010c1bd2f0222d5454
noxan <noxan@byteweaver.net>
2013-01-20 22:50:04 +0100

    add south to installed apps in settings

--- devisio/settings/__init__.py
+++ devisio/settings/__init__.py
@@ -114,6 +114,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # south
+    'south',
     # admin
     'grappelli',
     'filebrowser',
