cb3f8258bbf3af2dbc717e1ca40aa228f3c041f5
noxan <noxan@byteweaver.net>
2013-02-20 19:45:38 +0100

    add url for album detail view

--- devisio/photos/urls.py
+++ devisio/photos/urls.py
@@ -1,7 +1,8 @@
 from django.conf.urls import patterns, url
 
-from views import AlbumListView
+from views import AlbumDetailView, AlbumListView
 
 urlpatterns = patterns('',
     url('^$', AlbumListView.as_view(), name='list'),
+    url('^albums/(?P<pk>\d+)/$', AlbumDetailView.as_view(), name='detail'),
 )
