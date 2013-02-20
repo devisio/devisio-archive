efbca7b9610c64e9806ad0b957dea4bdb77a6387
noxan <noxan@byteweaver.net>
2013-02-20 19:47:55 +0100

    clean comments from django development settings

--- devisio/settings/development.py
+++ devisio/settings/development.py
@@ -5,12 +5,12 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'devisio',                      # Or path to database file if using sqlite3.
-        'USER': 'devisio',                      # Not used with sqlite3.
-        'PASSWORD': 'devisio',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'devisio',
+        'USER': 'devisio',
+        'PASSWORD': 'devisio',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
