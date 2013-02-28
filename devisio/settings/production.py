83846c02a1c504f0e7e8e9fa5b0e9a268cdd7f68
noxan <noxan@byteweaver.net>
2013-02-28 22:56:39 +0100

    remove comments from database production settings

--- devisio/settings/production.py
+++ devisio/settings/production.py
@@ -6,12 +6,12 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'devisio',                      # Or path to database file if using sqlite3.
-        'USER': 'devisio',                      # Not used with sqlite3.
-        'PASSWORD': '@8.b3D3&a^/r4,WTM5l4/*6}',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'devisio',
+        'USER': 'devisio',
+        'PASSWORD': '@8.b3D3&a^/r4,WTM5l4/*6}',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
