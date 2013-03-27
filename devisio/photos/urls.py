b016fcc226689d54ddf6893f2c122c74c9731486
noxan <noxan@byteweaver.net>
2013-03-27 21:43:43 +0100

    add url for overview view

--- devisio/photos/urls.py
+++ devisio/photos/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import patterns, url
 
-from views import AlbumJsonDetailView, AlbumListView
+from views import AlbumJsonDetailView, AlbumListView, OverviewView
 
 urlpatterns = patterns('',
     url('^$', AlbumListView.as_view(), name='list'),
+    url(r'^overview/$', OverviewView.as_view(), name='overview'),
     url('^albums/(?P<pk>\d+)/$', AlbumJsonDetailView.as_view(), name='detail'),
 )
