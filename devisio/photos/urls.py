091e9df3cbd009b210c327e183c874530d4ea55d
noxan <noxan@byteweaver.net>
2013-09-03 08:50:04 +0200

    add r for regex to photo urls conf

--- devisio/photos/urls.py
+++ devisio/photos/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from views import AlbumJsonDetailView, AlbumListView, OverviewView
 
 urlpatterns = patterns('',
-    url('^$', AlbumListView.as_view(), name='list'),
+    url(r'^$', AlbumListView.as_view(), name='list'),
     url(r'^overview/$', OverviewView.as_view(), name='overview'),
-    url('^albums/(?P<pk>\d+)/$', AlbumJsonDetailView.as_view(), name='detail'),
+    url(r'^albums/(?P<pk>\d+)/$', AlbumJsonDetailView.as_view(), name='detail'),
 )
