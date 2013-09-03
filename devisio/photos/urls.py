c7ea5622972fc543560eb5d153acdafd5cd2843e
noxan <noxan@byteweaver.net>
2013-09-03 08:52:12 +0200

    add shorter and better readable url for albums

--- devisio/photos/urls.py
+++ devisio/photos/urls.py
@@ -5,5 +5,6 @@ from views import AlbumJsonDetailView, AlbumListView, OverviewView
 urlpatterns = patterns('',
     url(r'^$', AlbumListView.as_view(), name='list'),
     url(r'^overview/$', OverviewView.as_view(), name='overview'),
+    url(r'^(?P<slug>[\w-]+)/$', AlbumJsonDetailView.as_view(), name='detail'),
     url(r'^albums/(?P<pk>\d+)/$', AlbumJsonDetailView.as_view(), name='detail'),
 )
