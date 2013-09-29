c6dfd774c22bf3fcc45c5d42b9dab4cf8600ee26
noxan <noxan@byteweaver.net>
2013-09-29 03:45:51 +0200

    add missing r to photos urls conf

--- devisio/photos/urls.py
+++ devisio/photos/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import patterns, url
 from views import AlbumDetailView, AlbumListView, AlbumOverviewView
 
 urlpatterns = patterns('',
-    url('^$', AlbumListView.as_view(), name='list'),
+    url(r'^$', AlbumListView.as_view(), name='list'),
     url(r'^overview/$', AlbumOverviewView.as_view(), name='overview'),
     url(r'^albums/(?P<slug>[\w-]+)/$', AlbumDetailView.as_view(), name='detail'),
 )
