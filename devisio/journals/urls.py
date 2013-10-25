90309bd22a38da93531810a80664532978e3c747
noxan <noxan@byteweaver.net>
2013-10-25 15:34:36 +0200

    reuse photos detail template to display photos of journal entry in its detail view

--- devisio/journals/urls.py
+++ devisio/journals/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import patterns, url
 
-from devisio.journals.views import JournalsListView, JournalsDetailView, JournalsTeaserView, JournalsMapView
+from devisio.journals.views import JournalsListView, JournalsDetailView, JournalsTeaserView, JournalsMapView, JournalEntryDetailView
 
 
 urlpatterns = patterns('',
     url(r'^$', JournalsListView.as_view(), name='list'),
     url(r'^map/$', JournalsMapView.as_view(), name='map'),
+    url(r'^(?P<pk>[\d]+)/$', JournalEntryDetailView.as_view(), name='entry'),
     url(r'^(?P<slug>[\w-]+)/$', JournalsDetailView.as_view(), name='detail'),
     url(r'^teaser/$', JournalsTeaserView.as_view(), name='teaser'),
 )
--- devisio/journals/views.py
+++ devisio/journals/views.py
@@ -1,7 +1,8 @@
 from django.views.generic import ListView, DetailView, TemplateView
 
+from devisio.photos.utils import serialize_photos_json
 from devisio.common.pjax import PJAXResponseMixin
-from devisio.journals.models import Journal
+from devisio.journals.models import Journal, JournalEntry
 
 
 class JournalsTeaserView(PJAXResponseMixin, TemplateView):
@@ -23,4 +24,10 @@ class JournalsDetailView(PJAXResponseMixin, DetailView):
 
 
 class JournalEntryDetailView(PJAXResponseMixin, DetailView):
-    pass
+    model = JournalEntry
+    template_name = 'photos/album_detail.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(JournalEntryDetailView, self).get_context_data(**kwargs)
+        context['json'] = serialize_photos_json(context['object'].photos.all())
+        return context
