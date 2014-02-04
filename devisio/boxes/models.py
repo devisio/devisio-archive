36c6b4a36a7daa92cad2790f84368f0f407f80ee
noxan <noxan@byteweaver.net>
2014-02-04 09:38:38 +0100

    override to string function in box model

--- devisio/boxes/models.py
+++ devisio/boxes/models.py
@@ -5,3 +5,6 @@ class Box(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
     description = models.TextField(blank=True, default='')
+
+    def __str__(self):
+        return self.title
