622419d6ec446e8da2e5bfc736674ee3ecb56855
noxan <noxan@byteweaver.net>
2014-02-05 12:26:07 +0100

    implement a box list view based on generic list view

--- devisio/boxes/views.py
+++ devisio/boxes/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from devisio.boxes.models import Box
+
+
+class BoxListView(ListView):
+    model = Box
