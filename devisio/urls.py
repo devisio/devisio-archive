703268abddf40b4d5bffc43b22d02a67dc29580f
noxan <noxan@byteweaver.net>
2014-03-29 16:45:33 +0100

    sort imports and remove sample from urls config

--- devisio/urls.py
+++ devisio/urls.py
@@ -1,12 +1,9 @@
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
-from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'devisio.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
