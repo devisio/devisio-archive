2b848cdbbac737e138632afd70e74778e51f3e0a
noxan <noxan@byteweaver.net>
2014-03-29 16:45:56 +0100

    provide base template for root url

--- devisio/urls.py
+++ devisio/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns('',
+    url(r'^$', TemplateView.as_view(template_name='base.html'), name='home'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
