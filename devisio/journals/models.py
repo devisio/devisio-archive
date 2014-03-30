8ba7114054d93b064b9f0b4edfdd130914892661
noxan <noxan@byteweaver.net>
2014-03-30 18:09:43 +0200

    override str function in journal to return title

--- devisio/journals/models.py
+++ devisio/journals/models.py
@@ -4,3 +4,6 @@ from django.db import models
 class Journal(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
