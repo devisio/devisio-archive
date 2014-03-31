58a7fe44d130873830c0819ee93c1c2d1b659b24
noxan <noxan@byteweaver.net>
2014-03-31 15:36:54 +0200

    add author to journal api

--- devisio/journals/api.py
+++ devisio/journals/api.py
@@ -4,6 +4,11 @@ from devisio.journals.models import Journal
 
 
 class JournalResource(ModelResource):
+    def dehydrate(self, bundle):
+        bundle.data['author'] = bundle.obj.author
+
+        return bundle
+
     class Meta:
         queryset = Journal.objects.all()
         #resource_name = 'journal'
