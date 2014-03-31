2c4ea674c49270926c5166632208971a46aa4c4d
noxan <noxan@byteweaver.net>
2014-03-31 14:35:34 +0200

    add api to urls

--- devisio/urls.py
+++ devisio/urls.py
@@ -2,8 +2,11 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
+from devisio.api import api1
+
 
 urlpatterns = patterns('',
+    url(r'^api/', include(api1.urls)),
 
     url(r'^admin/', include(admin.site.urls)),
 )
