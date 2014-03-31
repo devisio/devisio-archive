c6dd38368a77c93993ad7e0e92559aa3bbd0271d
noxan <noxan@byteweaver.net>
2014-03-31 14:19:08 +0200

    add author and creation_date fields to journal model

--- devisio/journals/models.py
+++ devisio/journals/models.py
@@ -1,9 +1,12 @@
+from django.conf import settings
 from django.db import models
 
 
 class Journal(models.Model):
     title = models.CharField(max_length=255)
     slug = models.SlugField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL)
+    creation_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.title
