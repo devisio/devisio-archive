8130553a4cd7be409a4f28c3c87e0a00b274fda8
noxan <noxan@byteweaver.net>
2014-04-17 19:12:03 +0200

    install and setup django debug toolbar

--- devisio/settings/development.py
+++ devisio/settings/development.py
@@ -3,3 +3,12 @@ from devisio.settings.base import *
 
 COMPRESS_ENABLED = False
 COMPRESS_OFFLINE = False
+
+
+INSTALLED_APPS += (
+    'debug_toolbar',
+)
+
+MIDDLEWARE_CLASSES += (
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
+)
